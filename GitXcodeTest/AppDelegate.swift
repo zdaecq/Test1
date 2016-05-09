4165880beb614b8cec35e7a1097ac5dcf5ad5745
zdaecqze zdaecq <zdaecq@mail.ru>
2016-05-09 15:44:10 +0300

    merged

--- GitXcodeTest/AppDelegate.swift
+++ GitXcodeTest/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        //dsfsdfsdf
+        ///sdfsdfsd
+        //sfdsgfdfgdfg
         
         
         // Override point for customization after application launch.
